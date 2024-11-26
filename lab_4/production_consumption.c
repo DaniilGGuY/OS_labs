cc9cd8b9ae5532db2f5301ef267c57647598e0c3
DaniilGGuY <daniiltuzov2004@gmail.com>
2024-11-26 22:24:48 +0300

    Fixed progs. Now it may be good to protect opposite of Ryazanova

--- lab_4/production_consumption.c
+++ lab_4/production_consumption.c
@@ -1,13 +1,16 @@
 #include <sys/sem.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <errno.h>
 
-#define BUF_SIZE       26
+#define BUF_SIZE       50
 #define SLEEP_TIMEP     2
-#define SLEEP_TIMEC     2
+#define SLEEP_TIMEC     3
 #define PRODUCERS       3
 #define CONSUMERS       4
 // Семафоры
@@ -18,69 +21,61 @@
 #define P              -1
 #define V               1
 
+int flag = 1;
+
 struct sembuf start_prod[2] = { { SEMPTY, P, 0 }, { SBIN, P, 0 } };
 struct sembuf stop_prod[2] = { { SBIN, V, 0 }, { SFULL, V, 0 } };
 struct sembuf start_cons[2] = { { SFULL, P, 0 }, { SBIN, P, 0 } };
 struct sembuf stop_cons[2] = { { SBIN, V, 0 }, { SEMPTY, V, 0} };
 
-void producer(const int shmid, const int semid) {
-    char *addr = shmat(shmid, NULL, 0);
-    if (addr == (char *)-1) {
-        perror("shmat");
-        exit(1);
-    }
+int signal_handler(int sig_numb) {
+    printf("PID %d signal %d\n", getpid(), sig_numb);
+    flag = 0;
+}
 
+void producer(char *addr, const int semid, char *alpha) {
     char **prod_ptr = addr;
     char **cons_ptr = prod_ptr + sizeof(char*);
     char *alpha_ptr = cons_ptr + sizeof(char*);
 
     srand(getpid());
-    while (1) {
+    while (flag) {
+        sleep(rand() % SLEEP_TIMEP);
         int semop_P = semop(semid, start_prod, 2);
         if (semop_P == -1) {
+            printf("PID %d blocked. Errno %d", getpid(), errno);
             perror("semop");
             exit(1);
         }
 
         **prod_ptr = *alpha_ptr;
-        printf("%d put %c\n", getpid(), **prod_ptr);
+        *alpha = **prod_ptr;
+        printf("%d put %c\n", getpid(), *alpha);
         ++(*prod_ptr);
         ++(*alpha_ptr);
         if (*alpha_ptr == 'z' + 1)
             *alpha_ptr = 'a';
 
-        sleep(rand() % SLEEP_TIMEP);
-
         int semop_V = semop(semid, stop_prod, 2);
         if (semop_V == -1) {
+            printf("PID %d blocked. Errno %d", getpid(), errno);
             perror("semop");
             exit(1);
         }
     }
-
-    if (shmdt((void *)prod_ptr) == -1) {
-        perror("shmdt");
-        exit(1);
-    }
-
-    exit(0);
 }
 
-void consumer(const int shmid, const int semid) {
-    char *addr = shmat(shmid, NULL, 0);
-    if (addr == (char *)-1) {
-        perror("shmat");
-        exit(1);
-    }
-
+void consumer(char *addr, const int semid) {
     char **prod_ptr = addr;
     char **cons_ptr = prod_ptr + sizeof(char*);
     char *alpha_ptr = cons_ptr + sizeof(char*);
 
     srand(getpid());
-    while (1) {
+    while (flag) {
+        sleep(rand() % SLEEP_TIMEC);
         int semop_P = semop(semid, start_cons, 2);
         if (semop_P == -1) {
+            printf("PID %d blocked. Errno %d", getpid(), errno);
             perror("semop");
             exit(1);
         }
@@ -88,38 +83,38 @@ void consumer(const int shmid, const int semid) {
         printf("%d get %c\n", getpid(), **cons_ptr);
         ++(*cons_ptr);
 
-        sleep(rand() % SLEEP_TIMEC);
-
         int semop_V = semop(semid, stop_cons, 2);
         if (semop_V == -1) {
+            printf("PID %d blocked. Errno %d", getpid(), errno);
             perror("semop");
             exit(1);
         }
     }
-
-    if (shmdt((void *)addr) == -1) {
-        perror("shmdt");
-        exit(1);
-    }
-
-    exit(0);
 }
 
 int main() {
+    struct sigaction sa = { 0 };
+    sa.sa_sigaction = &signal_handler;
+    if (sigaction(SIGINT, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
+    
     int semid, shmid;
     int perms = S_IRWXU | S_IRWXG | S_IRWXO;
 
     char **prod_ptr;
     char **cons_ptr;
     char *alpha_ptr;
+    char alpha;
 
-    if ((shmid = shmget(IPC_PRIVATE, (BUF_SIZE + 3) * sizeof(char), IPC_CREAT | perms)) == -1) {
+    if ((shmid = shmget(IPC_PRIVATE, 4096, IPC_CREAT | perms)) == -1) {
         perror("shmget");
         exit(1);
     }
 
     char *addr = shmat(shmid, NULL, 0);
-    if (addr == (char *)-1) {
+    if (addr == (void *)-1) {
         perror("shmat");
         exit(1);
     } 
@@ -145,7 +140,6 @@ int main() {
         exit(1);
     }
 
-
     pid_t childpid[PRODUCERS + CONSUMERS];
     
     for (int i = 0; i < PRODUCERS; ++i)
@@ -154,8 +148,10 @@ int main() {
             perror("fork");
             exit(1);
         }
-        if (childpid[i] == 0)
-            producer(shmid, semid);
+        if (childpid[i] == 0) {
+            producer(addr, semid, &alpha);
+            exit(0);
+        }
     }
 
     for (int i = PRODUCERS; i < CONSUMERS + PRODUCERS; ++i)
@@ -164,25 +160,33 @@ int main() {
             perror("fork");
             exit(1);
         }
-        if (childpid[i] == 0)
-            consumer(shmid, semid);
+        if (childpid[i] == 0) {
+            consumer(addr, semid);
+            exit(0);
+        }
     }
 
-    for (int i = 0; i < CONSUMERS + PRODUCERS; ++i) {
-        int wstatus;
-		pid_t w = waitpid(childpid[i], &wstatus, WUNTRACED | WCONTINUED);
+    pid_t w = 0;
+    int wstatus = 0;
+    while (w != -1) {
+        w = wait(&wstatus);  
         if (w == -1) {
-            perror("waitpid");
-            exit(EXIT_FAILURE);
-        }
-        if (WIFEXITED(wstatus)) {
-            printf("pid=%d exited, status=%d\n", w, WEXITSTATUS(wstatus));
-        } else if (WIFSIGNALED(wstatus)) {
-            printf("pid=%d killed by signal %d\n", w, WTERMSIG(wstatus));
-        } else if (WIFSTOPPED(wstatus)) {
-            printf("pid=%d stopped by signal %d\n", w, WSTOPSIG(wstatus));
-        } else if (WIFCONTINUED(wstatus)) {
-            printf("pid=%d continued\n", w);
+            if (errno == EINTR) {
+                w = 0;
+            } else if (errno != ECHILD) {
+                perror("wait");
+                exit(1);
+            }
+        } else {
+            if (WIFEXITED(wstatus)) {
+                printf("pid=%d exited, status=%d\n", w, WEXITSTATUS(wstatus));
+            } else if (WIFSIGNALED(wstatus)) {
+                printf("pid=%d killed by signal %d\n", w, WTERMSIG(wstatus));
+            } else if (WIFSTOPPED(wstatus)) {
+                printf("pid=%d stopped by signal %d\n", w, WSTOPSIG(wstatus));
+            } else if (WIFCONTINUED(wstatus)) {
+                printf("pid=%d continued\n", w);
+            }
         }
     }
 
--- lab_4/readers_writers.c
+++ lab_4/readers_writers.c
@@ -3,9 +3,12 @@
 #include <sys/ipc.h>
 #include <sys/stat.h>
 #include <sys/types.h>  
+#include <sys/wait.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <errno.h>
 
 #define READERS          5
 #define WRITERS          3
@@ -20,6 +23,8 @@
 #define P               -1
 #define V                1
 
+int flag = 1;
+
 struct sembuf start_read[4] = { { READERS_QUEUE, V, 0 }, 
 								{ WRITERS_QUEUE,  0, 0 }, 
 								{ ACTIVE_WRITERS,  0, 0 }, 
@@ -32,18 +37,18 @@ struct sembuf start_write[5] = { { WRITERS_QUEUE,  V, 0 },
 								 { ACTIVE_WRITERS,  V, 0 } };      
 struct sembuf stop_write[1]  = { { ACTIVE_WRITERS,  P, 0 } };
 
-void writer(const int shmid, const int semid) {
-    char *addr = shmat(shmid, NULL, 0);
-    if (addr == (char *)-1) {
-        perror("shmat");
-        exit(1);
-    }
+int signal_handler(int sig_numb) {
+    printf("PID %d signal %d\n", getpid(), sig_numb);
+    flag = 0;
+}
 
+void writer(char *addr, const int semid) {
     srand(getpid());
-    while (1) {
+    while (flag) {
         sleep(rand() % SLEEP_TIMEW);
         int semop_P = semop(semid, start_write, 5);
         if (semop_P == -1) {
+            printf("PID %d blocked", getpid());
             perror("semop");
             exit(1);
         }
@@ -55,31 +60,20 @@ void writer(const int shmid, const int semid) {
 
         int semop_V = semop(semid, stop_write, 1);
         if (semop_V == -1) {
+            printf("PID %d blocked", getpid());
             perror("semop");
             exit(1);
         }
     }
-
-    if (shmdt((void *)addr) == -1) {
-        perror("shmdt");
-        exit(1);
-    }
-
-    exit(0);
 }
 
-void reader(const int shmid, const int semid) {
-    char *addr = shmat(shmid, NULL, 0);
-    if (addr == (char *)-1) {
-        perror("shmat");
-        exit(1);
-    }
-
+void reader(char *addr, const int semid) {
     srand(getpid());
-    while (1) {
+    while (flag) {
         sleep(rand() % SLEEP_TIMER);
         int semop_P = semop(semid, start_read, 4);
         if (semop_P == -1) {
+            printf("PID %d blocked", getpid());
             perror("semop");
             exit(1);
         }
@@ -88,25 +82,26 @@ void reader(const int shmid, const int semid) {
 
         int semop_V = semop(semid, stop_read, 1);
         if (semop_V == -1) {
+            printf("PID %d blocked", getpid());
             perror("semop");
             exit(1);
         }
     }
-
-    if (shmdt((void *)addr) == -1) {
-        perror("shmdt");
-        exit(1);
-    }
-
-    exit(0);
 }
 
 int main(void)
 {
+    struct sigaction sa = { 0 };
+    sa.sa_sigaction = &signal_handler;
+    if (sigaction(SIGINT, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
+
 	int semid, shmid;
     int perms = S_IRWXU | S_IRWXG | S_IRWXO;
     
-    if ((shmid = shmget(IPC_PRIVATE, sizeof(char), IPC_CREAT | perms)) == -1) {
+    if ((shmid = shmget(IPC_PRIVATE, 4096, IPC_CREAT | perms)) == -1) {
         perror("shmget");
         exit(1);
     }
@@ -141,8 +136,10 @@ int main(void)
             perror("fork");
             exit(1);
         }
-        if (childpid[i] == 0)
-            writer(shmid, semid);
+        if (childpid[i] == 0) {
+            writer(addr, semid);
+            exit(0);
+        }
     }
 
     for (int i = WRITERS; i < READERS + WRITERS; ++i)
@@ -151,25 +148,33 @@ int main(void)
             perror("fork");
             exit(1);
         }
-        if (childpid[i] == 0)
-            reader(shmid, semid);
+        if (childpid[i] == 0) {
+            reader(addr, semid);
+            exit(0);
+        }
     }
 
-    for (int i = 0; i < READERS + WRITERS; ++i) {
-        int wstatus;
-		pid_t w = waitpid(childpid[i], &wstatus, WUNTRACED | WCONTINUED);
+    pid_t w = 0;
+    int wstatus = 0;
+    while (w != -1) {
+        w = wait(&wstatus);  
         if (w == -1) {
-            perror("waitpid");
-            exit(EXIT_FAILURE);
-        }
-        if (WIFEXITED(wstatus)) {
-            printf("pid=%d exited, status=%d\n", w, WEXITSTATUS(wstatus));
-        } else if (WIFSIGNALED(wstatus)) {
-            printf("pid=%d killed by signal %d\n", w, WTERMSIG(wstatus));
-        } else if (WIFSTOPPED(wstatus)) {
-            printf("pid=%d stopped by signal %d\n", w, WSTOPSIG(wstatus));
-        } else if (WIFCONTINUED(wstatus)) {
-            printf("pid=%d continued\n", w);
+            if (errno == EINTR) {
+                w = 0;
+            } else if (errno != ECHILD) {
+                perror("wait");
+                exit(1);
+            }
+        } else {
+            if (WIFEXITED(wstatus)) {
+                printf("pid=%d exited, status=%d\n", w, WEXITSTATUS(wstatus));
+            } else if (WIFSIGNALED(wstatus)) {
+                printf("pid=%d killed by signal %d\n", w, WTERMSIG(wstatus));
+            } else if (WIFSTOPPED(wstatus)) {
+                printf("pid=%d stopped by signal %d\n", w, WSTOPSIG(wstatus));
+            } else if (WIFCONTINUED(wstatus)) {
+                printf("pid=%d continued\n", w);
+            }
         }
     }
 
